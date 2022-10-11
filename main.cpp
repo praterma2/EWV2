e7e4b6090d8a54e2dad222d4f4e0e586034e748e
Matt Prater <praterma2@hotmail.com>
2022-10-10 19:50:15 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,186 @@
+/*
+ * LCDV1.cpp
+ *
+ * Created: 10/10/2022 2:26:51 PM
+ * Author : prate
+ */ 
+#define	F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>			/* Include Delay header file */
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LCD_Dir  DDRD			/* Define LCD data port direction */
+#define LCD_Port PORTD			/* Define LCD data port */
+#define RS PD2				/* Define Register Select pin */
+#define EN PD3 				/* Define Enable signal pin */
+#define DHT11_PIN PD1
+unsigned char text4[] = "KLMN";
+uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
+
+void Request()				/* Microcontroller send start pulse/request */
+{
+	DDRD |= (1<<DHT11_PIN);
+	PORTD &= ~(1<<DHT11_PIN);	/* set to low pin */
+	_delay_ms(20);			/* wait for 20ms */
+	PORTD |= (1<<DHT11_PIN);	/* set to high pin */
+}
+
+
+uint8_t Receive_data()			/* receive data */
+{
+	for (int q=0; q<8; q++)
+	{
+		while((PIND & (1<<DHT11_PIN)) == 0);  /* check received bit 0 or 1 */
+		_delay_us(30);
+		if(PIND & (1<<DHT11_PIN))/* if high pulse is greater than 30ms */
+		c = (c<<1)|(0x01);	/* then its logic HIGH */
+		else			/* otherwise its logic LOW */
+		c = (c<<1);
+		while(PIND & (1<<DHT11_PIN));
+	}
+	return c;
+}
+
+void LCD_Command( unsigned char cmnd )
+{
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); /* sending upper nibble */
+	LCD_Port &= ~ (1<<RS);		/* RS=0, command reg. */
+	LCD_Port |= (1<<EN);		/* Enable pulse */
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+
+	_delay_us(200);
+
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  /* sending lower nibble */
+	LCD_Port |= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+	_delay_ms(2);
+}
+
+
+void LCD_Char( unsigned char data )
+{
+	LCD_Port = (LCD_Port & 0x0F) | (data & 0xF0); /* sending upper nibble */
+	LCD_Port |= (1<<RS);		/* RS=1, data reg. */
+	LCD_Port|= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+
+	_delay_us(200);
+
+	LCD_Port = (LCD_Port & 0x0F) | (data << 4); /* sending lower nibble */
+	LCD_Port |= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+	_delay_ms(2);
+}
+
+void LCD_Init (void)			/* LCD Initialize function */
+{
+	LCD_Dir = 0xFF;			/* Make LCD port direction as o/p */
+	_delay_ms(20);			/* LCD Power ON delay always >15ms */
+	
+	LCD_Command(0x02);		/* send for 4 bit initialization of LCD  */
+	LCD_Command(0x28);              /* 2 line, 5*7 matrix in 4-bit mode */
+	LCD_Command(0x0c);              /* Display on cursor off*/
+	LCD_Command(0x06);              /* Increment cursor (shift cursor to right)*/
+	LCD_Command(0x01);              /* Clear display screen*/
+	_delay_ms(2);
+}
+
+
+void LCD_String (char *str)		/* Send string to LCD function */
+{
+	int i;
+	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	{
+		LCD_Char (str[i]);
+	}
+}
+
+void LCD_String_xy (char row, char pos, char *str)	/* Send string to LCD with xy position */
+{
+	if (row == 0 && pos<16)
+	LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
+	else if (row == 1 && pos<16)
+	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
+	LCD_String(str);		/* Call LCD string function */
+}
+
+void LCD_Pos_xy (char row, char pos)	/* Send string to LCD with xy position */
+{
+	if (row == 0 && pos<16)
+	LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
+	else if (row == 1 && pos<16)
+	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
+}
+void LCD_Clear()
+{
+	LCD_Command (0x01);		/* Clear display */
+	_delay_ms(2);
+	LCD_Command (0x80);		/* Cursor at home position */
+}
+
+void Response()				/* receive response from DHT11 */
+{
+	DDRD &= ~(1<<DHT11_PIN);
+	LCD_Char(text4[0]);
+	while(PIND & (1<<DHT11_PIN));
+	LCD_Char(text4[1]);
+	while((PIND & (1<<DHT11_PIN))==0);
+	LCD_Char(text4[2]);
+	while(PIND & (1<<DHT11_PIN));
+	LCD_Char(text4[3]);
+}
+
+
+int main(void)
+{
+    LCD_Init();			/* Initialization of LCD*/
+	unsigned char text1[] = "Temp: ";
+	unsigned char text2[] = "Humidity: ";
+	unsigned char text3[] = "efghij";
+int j = 0;
+	//writing temp
+	while(text1[j] != '\0')             //display text: Humidity
+	{
+		LCD_Char(text1[j]);
+		j++;
+	}
+    //LCD_Command(0xC0);		/* Go to 2nd line*/
+    LCD_Pos_xy(1,0);
+int i = 0;
+while(text2[i] != '\0')             //display text: Humidity
+{
+	LCD_Char(text2[i]);
+	 i++;
+}
+	//LCD_String("Hi");
+	
+	
+    while (1) 
+    {
+		LCD_Char(text3[0]);
+		Request();		/* send start pulse */
+		
+		Response();		/* receive response */
+		LCD_Char(text3[1]);
+		I_RH=Receive_data();	/* store first eight bit in I_RH */
+		D_RH=Receive_data();	/* store next eight bit in D_RH */
+		I_Temp=Receive_data();	/* store next eight bit in I_Temp */
+		D_Temp=Receive_data();	/* store next eight bit in D_Temp */
+		CheckSum=Receive_data();/* store next eight bit in CheckSum */
+
+	if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum)
+	{
+		LCD_Clear();
+		LCD_String("Error");
+	}
+
+	
+	_delay_ms(2000);
+    }
+}
+
